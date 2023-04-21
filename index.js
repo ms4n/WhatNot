bb32824692b6fc8bc51882fd4b7c7b000c52c690
Sanjay M <arjunsanjay0@gmail.com>
2023-04-21 15:55:06 +0530

    sending a message using axios

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
- 
+const axios = require("axios");
+const { config } = require("dotenv");
+config();
+
+const TEST_PHONE_NUMBER_ID = process.env.TEST_PHONE_NUMBER_ID;
+const API_ACCESS_TOKEN = process.env.API_ACCESS_TOKEN;
+const RECEIVER_PHONE_NUMBER = process.env.RECEIVER_PHONE_NUMBER;
+
+const url = `https://graph.facebook.com/v16.0/${TEST_PHONE_NUMBER_ID}/messages`;
+
+const headers = {
+  Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+  "Content-Type": "application/json",
+};
+
+const data = {
+  messaging_product: "whatsapp",
+  to: RECEIVER_PHONE_NUMBER,
+  type: "template",
+  template: { name: "hello_world", language: { code: "en_US" } },
+};
+
+axios
+  .post(url, data, { headers })
+  .then((response) => console.log(response.status, response.data))
+  .catch((error) => console.error(error));
