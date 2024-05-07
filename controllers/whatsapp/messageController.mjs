f53bea190d3faa65b8f4acf8400b78f27e0aa865
sanjay <arjunsanjay0@gmail.com>
2024-05-07 16:27:35 +0530

    code cleanup and comments added

--- controllers/whatsapp/messageController.mjs
+++ controllers/whatsapp/messageController.mjs
@@ -13,72 +13,92 @@ import {
   initializeDocsService,
   writeMessageToDocs,
 } from "../../services/googleServices/docsService.mjs";
+
 import {
   createCalendarEvent,
   initializeCalendarService,
 } from "../../services/googleServices/calendarService.mjs";
 
+// Object containing response messages for different scenarios
 const RESPONSE_MESSAGES = {
-  DEFAULT:
-    "Sorry, we currently only support text, image, document, audio, and video messages. If you haven't signed up to WhatNot App yet, you can do so here: https://whatnotapp.vercel.app/signup.",
   WELCOME_LINK:
     "Hello! 👋 Welcome to Whatnot! To get started, please sign up here: https://whatnotapp.vercel.app/signup. We're excited to have you on board!",
+  DEFAULT:
+    "Sorry, we currently only support text, image, document, audio, and video messages. If you haven't signed up to WhatNot App yet, you can do so here: https://whatnotapp.vercel.app/signup.",
 };
 
+// Function to handle incoming messages
 async function handleIncomingMessage(message) {
+  // Switch based on the type of the message
   switch (message.type) {
     case "text":
-      return handleTextMessage(message);
+      return handleTextMessage(message); // Handle text messages
     case "image":
     case "document":
     case "audio":
     case "video":
-      return handleMediaMessage(message);
+      return handleMediaMessage(message); // Handle media messages
     default:
-      return handleDefaultMessage(message);
+      return handleDefaultMessage(message); // Handle default messages
   }
 }
 
+// Function to handle text messages
 async function handleTextMessage(message) {
+  // Extract necessary information from the message
   const text = message.text.body.trim();
   const fromPhoneNumber = message.from;
   const timestamp = message.timestamp;
   const messageId = message.id;
 
   try {
+    // Check if the text message is a greeting
     if (text === "Hello! 👋") {
+      // Send a welcome message
       await sendTextMessage(fromPhoneNumber, RESPONSE_MESSAGES.WELCOME_LINK);
     } else if (text.toLowerCase().startsWith("/cal")) {
-      await initializeCalendarService(fromPhoneNumber);
+      // Check if the text message is a calendar event request
+      await initializeCalendarService(fromPhoneNumber); // Initialize Google Calendar service
 
+      // Create a calendar event based on the message content
       const eventResponse = await createCalendarEvent(text);
       if (eventResponse === 200) {
+        // If the event creation is successful, send a confirmation message
         await sendReactionMessage(fromPhoneNumber, messageId, "✅");
       }
     } else {
-      await initializeDriveService(fromPhoneNumber);
-      await initializeDocsService(fromPhoneNumber);
+      // If it's not a greeting or a calendar event, handle as a regular message
+      await initializeDriveService(fromPhoneNumber); // Initialize Google Drive service
+      await initializeDocsService(fromPhoneNumber); // Initialize Google Docs service
 
+      // Write the message to a Google Docs document
       const docsResposne = await writeMessageToDocs(text, timestamp);
       if (docsResposne === 200) {
+        // If writing to Docs is successful, send a confirmation message
         await sendReactionMessage(fromPhoneNumber, messageId, "✅");
       }
     }
   } catch (error) {
+    // Handle errors that occur during message handling
     console.error("Error occurred while handling text message:", error);
   }
 }
 
+// Function to handle media messages
 async function handleMediaMessage(message) {
+  // Extract necessary information from the message
   const mediaType = message.type;
   const mediaId = message[mediaType].id;
   const fromPhoneNumber = message.from;
   const messageId = message.id;
 
   try {
+    // Initialize Google Drive service
     await initializeDriveService(fromPhoneNumber);
+    // Get the media object from its ID
     const mediaObject = await getMediaObjectFromId(mediaId);
 
+    // Handle media upload to Google Drive
     const mediaUploadResponse = await handleWhatsAppMediaUpload(
       mediaObject,
       mediaType,
@@ -86,16 +106,21 @@ async function handleMediaMessage(message) {
     );
 
     if (mediaUploadResponse === 200) {
+      // If media upload is successful, send a confirmation message
       await sendReactionMessage(fromPhoneNumber, messageId, "✅");
     }
   } catch (error) {
+    // Handle errors that occur during media handling
     console.error(error);
     throw error;
   }
 }
 
+// Function to handle default messages
 async function handleDefaultMessage(message) {
+  // Extract necessary information from the message
   const fromPhoneNumber = message.from;
+  // Send a default response message
   await sendTextMessage(fromPhoneNumber, RESPONSE_MESSAGES.DEFAULT);
 }
 
--- services/googleServices/docsService.mjs
+++ services/googleServices/docsService.mjs
@@ -1,28 +1,33 @@
-import { findOrCreateFile } from "./driveService.mjs";
-import { getDocsService } from "../../config/googleApiConfig.mjs";
+import { findOrCreateFile } from "./driveService.mjs"; 
+import { getDocsService } from "../../config/googleApiConfig.mjs"; 
 
-let docsService;
+let docsService; // Variable to store the initialized Google Docs service
 
+// Function to initialize the Google Docs service
 async function initializeDocsService(phoneNumber) {
   try {
+    // Get the Google Docs service using the provided phone number
     docsService = await getDocsService(phoneNumber);
   } catch (err) {
+    // Handle errors that occur during initialization
     console.error("Error initializing Docs service:", err);
-    throw err;
+    throw err; // Rethrow the error to be handled by the caller
   }
 }
 
+// Function to write a message to a Google Docs document
 async function writeMessageToDocs(message, timestamp) {
   try {
+    // Define metadata for the Google Docs file
     const fileMetadata = {
       name: "Whatsapp NoteSync",
       mimeType: "application/vnd.google-apps.document",
     };
 
+    // Find or create the Google Docs file
     const docFile = await findOrCreateFile(fileMetadata);
 
-    // console.log(docFile);
-
+    // Format the timestamp into human-readable date and time
     const date = new Date(timestamp * 1000);
     const formattedDate = date.toLocaleDateString("en-us", {
       weekday: "long",
@@ -30,31 +35,33 @@ async function writeMessageToDocs(message, timestamp) {
       month: "short",
       day: "numeric",
     });
-
-    // add hours and minutes:
     const hours = ("0" + date.getHours()).slice(-2);
     const minutes = ("0" + date.getMinutes()).slice(-2);
     const formattedTime = `${hours}:${minutes}`;
 
+    // Define requests to insert text into the Google Docs file
     const requests = [
       {
         insertText: {
-          endOfSegmentLocation: { segmentId: "" }, // Appends to the body segment
+          endOfSegmentLocation: { segmentId: "" },
           text: `Date: ${formattedTime}, ${formattedDate} \n${message}\n\n`,
         },
       },
     ];
 
+    // Batch update the Google Docs document with the message
     const doc = await docsService.documents.batchUpdate({
       documentId: docFile,
       resource: { requests: requests },
     });
 
+    // Return the status of the document update
     return doc.status;
   } catch (error) {
+    // Handle errors that occur during writing to Google Docs
     console.error("Error writing message to Google Docs:", error);
-    throw error; // rethrow the error to be handled by the caller
+    throw error; // Rethrow the error to be handled by the caller
   }
 }
 
-export { initializeDocsService, writeMessageToDocs };
+export { initializeDocsService, writeMessageToDocs }; 
