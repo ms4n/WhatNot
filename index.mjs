cd42d2314c1014153859b2018b082f562ded840b
sanjay <arjunsanjay0@gmail.com>
2024-05-10 14:33:45 +0530

    session saveUninitialized to false

--- index.mjs
+++ index.mjs
@@ -25,7 +25,7 @@ app.use(
     store: new RedisStore({ client: client }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
   })
 );
 
